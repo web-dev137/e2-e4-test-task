c94bb32257ed30513fea98df99de49bd01c3973b
Daniyar <daniyar.tuletsov.1994@mail.ru>
2024-03-10 14:25:51 +0700

    add constant for toggle mode

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 use App\utils\App;
 require_once __DIR__."/vendor/autoload.php";
 $config = include __DIR__."/config/config.php";
-App::setMode(true);
+const APP_MODE_TEST = false;
+App::setMode(APP_MODE_TEST);
 (new App())->run($config);
 
